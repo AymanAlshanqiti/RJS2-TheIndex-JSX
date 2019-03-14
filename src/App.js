f2fc23159e8dafd9b384f850db54b7f67ce75ed3
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-03-14 21:31:38 +0300

    Re do task 2 to practising JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import authors from "./data";
 
 class App extends Component {
   render() {
+    const authorsList = authors.map(author => {
+      const authorName = `${author.first_name} ${author.last_name}`;
+      return (
+        <div key={authorName} className="col-lg-4 col-md-6 col-12">
+          <div className="card">
+            <div className="image">
+              <img
+                className="card-img-top img-fluid"
+                src={author.imageUrl}
+                alt={authorName}
+              />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">
+                <span>{authorName}</span>
+              </h5>
+              <small className="card-text">{author.books.length} books</small>
+            </div>
+          </div>
+        </div>
+      );
+    });
     return (
-      <div>
+      <div id="app" className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+            <div id="sidebar">
+              <img src="theindex.svg" className="logo" alt="the index logo" />
+              <section>
+                <h4 className="menu-item active">
+                  <button>AUTHORS</button>
+                </h4>
+              </section>
+            </div>
+          </div>
+          <div className="content col-10">
+            <div className="authors">
+              <h3>Authors</h3>
+              <div className="row">{authorsList}</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
